c0556eaa7e26d2b9290aca1ea59b2121b6c8a0cc
Rafi Fauzan <rafifauzan976@gmail.com>
2024-02-05 22:20:08 +0800

    Authorization

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -24,4 +24,20 @@
         </form>
       </li>
     </ul>
+
+    @can('admin')
+    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+      <span>Administrator</span>
+    </h6>
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard/categories*') ? '' : 'text-dark' }}" href="/dashboard/categories">
+          <svg class="bi"><use xlink:href="#file-earmark-text"/></svg>
+          Post Categories
+        </a>
+      </li>
+    </ul>
+    @endcan
+    
+
   </div>
